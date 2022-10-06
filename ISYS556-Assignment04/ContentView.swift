44773a4cafd89847e2ec813e2a68616aa9194cf0
Patrick Mims <patrickkevinmims@gmail.com>
2022-10-06 13:13:27 -0700

    Initial Commit

---
+++ ISYS556-Assignment04/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ISYS556-Assignment04
+//
+//  Created by JPL-ST-SPRING2022 on 10/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ISYS556-Assignment04/ISYS556_Assignment04App.swift
@@ -0,0 +1,17 @@
+//
+//  ISYS556_Assignment04App.swift
+//  ISYS556-Assignment04
+//
+//  Created by JPL-ST-SPRING2022 on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ISYS556_Assignment04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
